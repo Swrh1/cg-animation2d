100aea8aa3d9832a5dcf9e7535a147eb7283ba11
Eldredge <eldr1884@STTHOMAS.EDU>
2023-02-24 09:19:27 -0600

    Work on slide 1

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -109,8 +109,25 @@ class Renderer {
     drawSlide1() {
         // TODO: draw at least 3 polygons that spin about their own centers
         //   - have each polygon spin at a different speed / direction
+        let square = [Vector3(100, 100, 1),
+                      Vector3(300, 100, 1),
+                      Vector3(300, 300, 1),
+                      Vector3(100, 300, 1)];
+        //this.drawConvexPolygon(square, [255, 0, 0, 255]);
         
-        
+        let square_origin = new Matrix(3, 3);
+        mat3x3Translate(square_origin, -200, -200);
+        let square_rotate = new Matrix(3, 3);
+        mat3x3Rotate(square_rotate, 45);
+        let square_return = new Matrix(3, 3);
+        mat3x3Translate(square_return, 200, 200);
+        let mult1 = Matrix.multiply([square_return, square_rotate]);
+        let mult2 = Matrix.multiply([mult1, square_origin]);
+        for(let i = 0; i < square.length; i++) {
+            square[i] = Matrix.multiply([mult2, square[i]]);
+        }
+        console.log(square);
+        this.drawConvexPolygon(square, [255, 0, 0, 255]);
     }
 
     //
